53fbc2646c7942efde14031d3920ed380c69aad9
saidatla201 <saidatla201@gmail.com>
2023-02-20 18:45:41 +0530

    Adding EvenOrodd program

---
+++ EvenOrOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class EvenOrOdd
+    {
+        public static void CheckNumberEvenorOdd()
+        {
+            int num = 0;
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even number: " + num);
+            }
+            else
+            {
+                Console.WriteLine("Odd Number: " + num);
+            }
+        }
+    }
+}
