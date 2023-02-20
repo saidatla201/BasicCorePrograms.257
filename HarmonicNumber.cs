ca8968d37c323f713091f13ec020e3a1d8ea547c
saidatla201 <saidatla201@gmail.com>
2023-02-20 19:04:20 +0530

    adding Harmonic Number to Basic core Program

---
+++ HarmonicNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class HarmonicNumber
+    {
+        public static void HarmonicNum()
+        {
+            double sum = 0;
+            Console.WriteLine("Enter a Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                sum = sum + 1 / (float)i;
+            }
+            Console.WriteLine("\nHarmonic value of {0} term is : {1}", num, sum);
+        }
+    }
+}
+
