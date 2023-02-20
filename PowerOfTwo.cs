5e494e2d3dea0a6b38357ac8fdc183a023398d25
saidatla201 <saidatla201@gmail.com>
2023-02-20 19:41:31 +0530

    Adding PowerOfTwo Programm

---
+++ PowerOfTwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public  class PowerOfTwo
+    {
+        public static void PowerOf2()
+        {
+            Console.WriteLine("Enter a Number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num && num <= 10; i++)
+            {
+                double result = Math.Pow(2, i);
+                Console.WriteLine("{0}'s Power {1} : {2}", 2, i, result);
+            }
+        }
+    }
+}
