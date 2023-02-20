c36adab27a673a17484f925e405f02a26e668b2a
saidatla201 <saidatla201@gmail.com>
2023-02-20 18:56:56 +0530

    Adding LeapYear program to Basic core programs

---
+++ LeapYear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class LeapYear
+    {
+        public static void Leapyear()
+        {
+            Console.WriteLine("Enter the year ");
+
+            
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if  ((year % 400 == 0) || (year % 100 != 0) && (year % 4 == 0))
+            {
+                Console.WriteLine("The given year is a LeapYear");
+            }
+            else
+            {
+                Console.WriteLine("The given year is not a Leap Year");
+            }
+        }
+
+        
+    }
+}
