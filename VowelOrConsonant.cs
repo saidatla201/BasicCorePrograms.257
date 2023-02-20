b88821ac75ef99029be1b188a1f65471a89c38e4
saidatla201 <saidatla201@gmail.com>
2023-02-20 19:35:58 +0530

    Adding VowelOrConsonant Program

---
+++ VowelOrConsonant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class VowelOrConsonant
+    {
+        public static void VowelOrConst()
+        {
+            Console.WriteLine("Enter a character: ");
+            Char ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine("Entered character {0} is a vowel.", ch);
+
+            }
+            else
+            {
+                Console.WriteLine("Entered Character {0} is a consonant.", ch);
+            }
+        }
+    }
+}
